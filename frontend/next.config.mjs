ee09eeb65c15ce3c471ed907089ab15e49a2b217
Finn <89312393+finnzxje@users.noreply.github.com>
2025-12-11 23:12:31 +0700

    test: write local infrastructure for testing

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/:path*',
+        destination: `${process.env.API_URL || 'http://localhost:8080'}/:path*`,
       },
     ];
   },
